9190aba6f6577d04d836076a33b89945e47a77c4
uusa35 <uusa35@gmail.com>
2021-08-13 14:41:06 +0300

    updates

--- resources/js/Pages/Frontend/HomePage.js
+++ resources/js/Pages/Frontend/HomePage.js
@@ -21,62 +21,77 @@ export default function HomePage({auth, settings }) {
 
     return (
         <div className="relative bg-gray-50 overflow-hidden">
-            <Head title={capitalize(settings[getLocalized()])}>
-                <meta head-key="title" name="title" content={settings[getLocalized()]}/>
-                <meta head-key="description" name="description" content={settings[getLocalized('description')]}/>
-                <link href={getImageThumb(settings.logo)} rel="shortcut icon" type="image/png"/>
-                <link rel="canonical"  href={route('home')}/>
-                <link rel="icon" type="image/svg+xml" href={getImageThumb(settings.image)}/>
-                <meta
-                    http-equiv="Content-type"
-                    charSet="utf-8"
-                    content="text/html; charset=utf-8"
-                />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>{settings[getLocalized()]}</title>
-                <meta name="title" content={settings[getLocalized()]}/>
-                <link href={getImageThumb(settings.image)} rel="shortcut icon" type="image/png"/>
-                <meta name={settings[getLocalized()]} content="E-commerce"/>
-                <meta
-                    key="theme-color"
-                    name="theme-color"
-                    content={settings.theme}
-                />
-                <meta key="keywords" name="keywords" content={settings[getLocalized()]}/>
-                <meta key="author" name="author" content={settings[getLocalized()]}/>
-                <meta key="country" name="country" content={settings[getLocalized('country')]}/>
-                <meta key="mobile" name="mobile" content={settings.mobile}/>
-                <meta key="whatsapp" name="whatsapp" content={settings.whatsapp}/>
-                <meta key="phone" name="phone" content={settings.phone}/>
-                <meta key="logo" name="logo" content={getImageThumb(settings.image)}/>
-                <meta key="email" name="email" content={settings.email}/>
-                <meta key="address" name="address" content={settings[getLocalized('address')]}/>
-                <meta key="name" name="name" content={settings[getLocalized()]}/>
-                <meta key="lang" name="lang" content={locale}/>
-                <meta
-                    name="description"
-                    key="description"
-                    content={`${settings[getLocalized('description')]}`}
-                />
-                <meta itemProp="name" content={settings[getLocalized()]}/>
-                <meta itemProp="description" content={`${settings[getLocalized('description')]}`}/>
-                <meta itemProp="image" content={getImageThumb(settings.image)}/>
-                <meta property="og:type" content="website" key="ogtype"/>
-                <meta
-                    property="og:site_name"
-                    content={`${settings[getLocalized()]}`}
-                    key="ogsitename"
-                />
-                <meta property="og:url" content={settings.apple} key="ogurl"/>
-                <meta
-                    property="og:title"
-                    content={`${settings[getLocalized()]}`}
-                    key="ogtitle"
-                />
-                <meta property="og:description" content={settings[getLocalized('description')]} key="ogdesc"/>
-                <meta property="og:image" content={getImageThumb(settings.image)} key="ogimage"/>
+            <Head>
+                <title>Inertia Head Demo</title>
+                <meta type="description" content="Inertia Head Demo." />
+                <meta name="twitter:title" content="Inertia Head Demo" />
+                <meta name="twitter:site" content="@ninjaparade" />
+                <meta name="twitter:creator" content="@ninjaparade" />
+                <meta name="twitter:description" content="Inertia Head Demo." />
+                <meta name="twitter:card" content="summary_large_image" />
+
+                <meta property="og:url" content="http://inertia-head-demo.test" />
+                <meta property="og:title" content="Inertia Head Demo" />
+                <meta property="og:description" content="Inertia Head Demo" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="https://www.biography.com/.image/t_share/MTQ3NTI2OTA4NzY5MjE2MTI4/drake_photo_by_prince_williams_wireimage_getty_479503454.jpg" />
             </Head>
+            {/*<Head title={capitalize(settings[getLocalized()])}>*/}
+            {/*    <meta head-key="title" name="title" content={settings[getLocalized()]}/>*/}
+            {/*    <meta head-key="description" name="description" content={settings[getLocalized('description')]}/>*/}
+            {/*    <link href={getImageThumb(settings.logo)} rel="shortcut icon" type="image/png"/>*/}
+            {/*    <link rel="canonical"  href={route('home')}/>*/}
+            {/*    <link rel="icon" type="image/svg+xml" href={getImageThumb(settings.image)}/>*/}
+            {/*    <meta*/}
+            {/*        http-equiv="Content-type"*/}
+            {/*        charSet="utf-8"*/}
+            {/*        content="text/html; charset=utf-8"*/}
+            {/*    />*/}
+            {/*    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>*/}
+            {/*    <meta name="viewport" content="width=device-width, initial-scale=1"/>*/}
+            {/*    <title>{settings[getLocalized()]}</title>*/}
+            {/*    <meta name="title" content={settings[getLocalized()]}/>*/}
+            {/*    <link href={getImageThumb(settings.image)} rel="shortcut icon" type="image/png"/>*/}
+            {/*    <meta name={settings[getLocalized()]} content="E-commerce"/>*/}
+            {/*    <meta*/}
+            {/*        key="theme-color"*/}
+            {/*        name="theme-color"*/}
+            {/*        content={settings.theme}*/}
+            {/*    />*/}
+            {/*    <meta key="keywords" name="keywords" content={settings[getLocalized()]}/>*/}
+            {/*    <meta key="author" name="author" content={settings[getLocalized()]}/>*/}
+            {/*    <meta key="country" name="country" content={settings[getLocalized('country')]}/>*/}
+            {/*    <meta key="mobile" name="mobile" content={settings.mobile}/>*/}
+            {/*    <meta key="whatsapp" name="whatsapp" content={settings.whatsapp}/>*/}
+            {/*    <meta key="phone" name="phone" content={settings.phone}/>*/}
+            {/*    <meta key="logo" name="logo" content={getImageThumb(settings.image)}/>*/}
+            {/*    <meta key="email" name="email" content={settings.email}/>*/}
+            {/*    <meta key="address" name="address" content={settings[getLocalized('address')]}/>*/}
+            {/*    <meta key="name" name="name" content={settings[getLocalized()]}/>*/}
+            {/*    <meta key="lang" name="lang" content={locale}/>*/}
+            {/*    <meta*/}
+            {/*        name="description"*/}
+            {/*        key="description"*/}
+            {/*        content={`${settings[getLocalized('description')]}`}*/}
+            {/*    />*/}
+            {/*    <meta itemProp="name" content={settings[getLocalized()]}/>*/}
+            {/*    <meta itemProp="description" content={`${settings[getLocalized('description')]}`}/>*/}
+            {/*    <meta itemProp="image" content={getImageThumb(settings.image)}/>*/}
+            {/*    <meta property="og:type" content="website" key="ogtype"/>*/}
+            {/*    <meta*/}
+            {/*        property="og:site_name"*/}
+            {/*        content={`${settings[getLocalized()]}`}*/}
+            {/*        key="ogsitename"*/}
+            {/*    />*/}
+            {/*    <meta property="og:url" content={settings.apple} key="ogurl"/>*/}
+            {/*    <meta*/}
+            {/*        property="og:title"*/}
+            {/*        content={`${settings[getLocalized()]}`}*/}
+            {/*        key="ogtitle"*/}
+            {/*    />*/}
+            {/*    <meta property="og:description" content={settings[getLocalized('description')]} key="ogdesc"/>*/}
+            {/*    <meta property="og:image" content={getImageThumb(settings.image)} key="ogimage"/>*/}
+            {/*</Head>*/}
             <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
                 <div className="relative h-full max-w-7xl mx-auto">
                     <svg
