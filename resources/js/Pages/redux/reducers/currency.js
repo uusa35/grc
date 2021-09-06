0a227200acab2db6f258b09feb4ef6260bffc575
uusa35 <uusa35@gmail.com>
2021-09-06 22:33:07 +0300

    updates

--- resources/js/Pages/redux/reducers/currency.js
+++ resources/js/Pages/redux/reducers/currency.js
@@ -1,6 +1,13 @@
-import { SET_CURRENCY} from "../actions/types";
+import {SET_CURRENCY} from "../actions/types";
 
-export default function(currency =  {} , action) {
+export default function(currency= {
+    currency_symbol_ar: "د.ك",
+    currency_symbol_en: "KWD",
+    exchange_rate: 1,
+    image: "KWT.png",
+    name_ar: " دينار كويتي",
+    name_en: "Kuwait Dinar"
+}, action) {
     switch (action.type) {
         case SET_CURRENCY:
             return action.payload;
