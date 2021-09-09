254b785403d7955fd7994d07de4a25815b13e732
uusa35 <uusa35@gmail.com>
2021-09-09 10:09:03 +0300

    updates

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,10 @@
-@php
-    try {
-        $ssr = Http::get('http://ecommerce-backend.test/render', $page)->throw()->json();
-    } catch (Exception $e) {
-        $ssr = null;
-    }
-@endphp
+{{--@php--}}
+{{--    try {--}}
+{{--        $ssr = Http::get('http://ecommerce-backend.test/render', $page)->throw()->json();--}}
+{{--    } catch (Exception $e) {--}}
+{{--        $ssr = null;--}}
+{{--    }--}}
+{{--@endphp--}}
     <!DOCTYPE html>
 <html>
 <head>
@@ -76,20 +76,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    @foreach($ssr['head'] ?? [] as $element)
-        {!! $element !!}
-    @endforeach
+{{--    @foreach($ssr['head'] ?? [] as $element)--}}
+{{--        {!! $element !!}--}}
+{{--    @endforeach--}}
 </head>
 <body>
 <div id="lang" style="display: none">{{ session()->get('lang') }}</div>
 <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
     @csrf
 </form>
-@if ($ssr)
-    {!! $ssr['body'] !!}
-@else
+{{--@if ($ssr)--}}
+{{--    {!! $ssr['body'] !!}--}}
+{{--@else--}}
     @routes
     @inertia
-@endif
+{{--@endif--}}
 </body>
 </html>
