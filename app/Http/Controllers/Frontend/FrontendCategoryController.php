c744428f66f318752aa1115ace42fcd024d7ca39
uusa35 <uusa35@gmail.com>
2021-10-25 02:25:57 +0200

    updates

--- app/Http/Controllers/Frontend/FrontendCategoryController.php
+++ app/Http/Controllers/Frontend/FrontendCategoryController.php
@@ -21,7 +21,7 @@ class FrontendCategoryController extends Controller
         if ($validator->fails()) {
             return response()->json(['message' => $validator->errors()->first()], 400);
         }
-        $elements = CategoryCollection::make(Category::filters($filters)->orderBy('id', 'desc')
+        $elements = CategoryCollection::make(Category::active()->filters($filters)->orderBy('id', 'desc')
             ->paginate(Self::TAKE_MIN)->withQueryString());
         return inertia('Frontend/Category/FrontendCategoryIndex', compact('elements'));
     }
