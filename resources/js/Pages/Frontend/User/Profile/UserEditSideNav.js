896654acc0db71d96a1882ac2f9dd4c6574ba8de
uusa35 <uusa35@gmail.com>
2021-11-11 01:41:05 +0200

    updates

--- resources/js/Pages/Frontend/User/Profile/UserEditSideNav.js
+++ resources/js/Pages/Frontend/User/Profile/UserEditSideNav.js
@@ -145,24 +145,26 @@ export default function() {
                     <span className="truncate">{trans('my_favorites')}</span>
                 </Link>
                 }
-                <Link
-                    href={route('frontend.profile.setting')}
-                    className={classNames(
-                        route().current() === 'frontend.profile.setting'
-                            ? 'text-gray-900 bg-gray-100 hover:bg-gray-100 hover:text-gray-300'
-                            : 'border-transparent text-gray-400 hover:bg-gray-50 hover:text-gray-900',
-                        'group border-l-4 px-3 py-2 flex items-center text-sm font-medium rounded-md'
-                    )}
-                    aria-current={true ? 'page' : undefined}
-                >
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         className="h-6 w-6 mx-2"
-                         fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
-                    <span className="truncate">{trans('my_settings')}</span>
-                </Link>
+                {
+                    settings.enable_newsletter && <Link
+                        href={route('frontend.profile.setting')}
+                        className={classNames(
+                            route().current() === 'frontend.profile.setting'
+                                ? 'text-gray-900 bg-gray-100 hover:bg-gray-100 hover:text-gray-300'
+                                : 'border-transparent text-gray-400 hover:bg-gray-50 hover:text-gray-900',
+                            'group border-l-4 px-3 py-2 flex items-center text-sm font-medium rounded-md'
+                        )}
+                        aria-current={true ? 'page' : undefined}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                             className="h-6 w-6 mx-2"
+                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <span className="truncate">{trans('my_settings')}</span>
+                    </Link>
+                }
             </nav>
         </aside>
     );
