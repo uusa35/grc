b44079cdc3ab29282a02ce9982cce77872c4dcb2
uusa35 <uusa35@gmail.com>
2021-11-22 12:55:08 +0200

    updates

--- resources/js/Pages/Frontend/Book/FrontendBookShow.js
+++ resources/js/Pages/Frontend/Book/FrontendBookShow.js
@@ -244,7 +244,7 @@ export default function({element, relatedElements, auth}) {
                                 </h2>
                                 <div className="border-t divide-y divide-gray-200 ">
                                     {/* description */}
-                                    {element[getLocalized('notes')] && element[getLocalized('description')] > 5 ?
+                                    {element[getLocalized('description')] && element[getLocalized('description')] > 5 ?
                                         <Disclosure as="div" defaultOpen={true}>
                                             {({open}) => (
                                                 <>
