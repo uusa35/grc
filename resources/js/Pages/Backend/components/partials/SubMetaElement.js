7c3472504ad0792d379e26539e885f57951b470f
uusa35 <uusa35@gmail.com>
2022-01-06 14:49:36 +0300

    updates

--- resources/js/Pages/Backend/components/partials/SubMetaElement.js
+++ resources/js/Pages/Backend/components/partials/SubMetaElement.js
@@ -1,31 +1,15 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import {InertiaHead} from "@inertiajs/inertia-react";
-import {AppContext} from "../../../context/AppContext";
-import GlobalContext from "../../../context/GlobalContext";
 
 const SubMetaElement = ({
                             title = null,
                             image = null,
                             description = null,
                         }) => {
-    const { getThumb } = useContext(AppContext);
-    const { settings } = useContext(GlobalContext);
-
     return (
         <InertiaHead>
             <title>{title}</title>
             <meta name="title" content={title}/>
-            <meta  name="description"
-                  content={description}/>
-            <meta itemProp="image" content={getThumb(image)}/>
-            <meta property="og:type" content="website" key="ogtype"/>
-            <meta
-                property="og:title"
-                content={title}
-                key="ogtitle"
-            />
-            <meta property="og:description" content={description} key="ogdesc"/>
-            <meta property="og:image" content={getThumb(image)} key="ogimage"/>
         </InertiaHead>
     );
 };
