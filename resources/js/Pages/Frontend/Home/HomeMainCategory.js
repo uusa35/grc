5e9a535482d586d4f4e24ab566776abb89c6071e
uusa35 <uusa35@gmail.com>
2022-01-30 10:29:16 +0300

    updates

--- resources/js/Pages/Frontend/Home/HomeMainCategory.js
+++ resources/js/Pages/Frontend/Home/HomeMainCategory.js
@@ -22,7 +22,7 @@ export default function({element}) {
                     <div aria-hidden="true" className="relative w-full h-96 lg:hidden"/>
                     <div aria-hidden="true" className="relative w-full h-32 lg:hidden"/>
                     <div
-                        className={classNames(locale.isRTL ? `left-0` :  `right-0` , "absolute inset-y-0 bottom-0 bg-black bg-opacity-5 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur sm:flex sm:items-center sm:justify-between  lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start")}>
+                        className={classNames(locale.isRTL ? `left-0` :  `right-0` , "absolute inset-y-0 bottom-0 bg-black bg-opacity-75 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur-sm sm:flex sm:items-center sm:justify-between  lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start")}>
                         <div>
                             <h2 className="text-xl font-bold text-white">{element[getLocalized()]}</h2>
                             {
