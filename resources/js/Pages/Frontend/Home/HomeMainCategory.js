420368e3b6c2c7327ab5a2e6e43e531f1ec32042
uusa35 <uusa35@gmail.com>
2022-01-30 13:49:54 +0300

    updates

--- resources/js/Pages/Frontend/Home/HomeMainCategory.js
+++ resources/js/Pages/Frontend/Home/HomeMainCategory.js
@@ -1,5 +1,5 @@
 /* This example requires Tailwind CSS v2.0+ */
-import {useContext, useState} from "react";
+import React, {useContext} from "react";
 import {AppContext} from "../../context/AppContext";
 import {Link} from "@inertiajs/inertia-react";
 import route from 'ziggy-js'
