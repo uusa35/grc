d7712b15c0c799e13d6b3888558881d5d00f5542
uusa35 <uusa35@gmail.com>
2022-02-01 11:20:01 +0300

    updates

--- resources/js/Pages/redux/reducers/searchType.js
+++ resources/js/Pages/redux/reducers/searchType.js
@@ -1,6 +1,6 @@
 import { SET_SEARCH_TYPE} from "../actions/types";
 
-export default function(searchType = null, action) {
+export default function(searchType = 'user', action) {
     switch (action.type) {
         case SET_SEARCH_TYPE:
             return action.payload;
