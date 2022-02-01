c3ecaa51e13eed1e4f119e4aa91f072a7c5ea69d
uusa35 <uusa35@gmail.com>
2022-02-01 13:08:28 +0300

    updates

--- resources/js/Pages/Frontend/Home/HomeMainCategory.js
+++ resources/js/Pages/Frontend/Home/HomeMainCategory.js
@@ -28,17 +28,17 @@ const HomeMainCategory = ({element = {}}) => {
                     <div
                         className={classNames(locale.isRTL ? `left-0` :  `right-0` , "absolute inset-y-0 bottom-0 bg-black bg-opacity-5 p-6 rounded-bl-lg rounded-br-lg backdrop-filter backdrop-blur-sm sm:flex sm:items-center sm:justify-between  lg:w-96 lg:rounded-tl-lg lg:rounded-br-none lg:flex-col lg:items-start")}>
                         <div>
-                            <h2 className="text-xl font-bold text-white">{element[getLocalized()]}</h2>
+                            <h2 className="text-xl font-bold text-white truncate capitalize">{element[getLocalized()]}</h2>
                             {
                                 element[getLocalized('description')] && element[getLocalized('description')].length > 5 &&
-                                <p className="mt-1 text-sm text-gray-300">
+                                <p className="mt-1 text-sm text-white font-bold truncate capitalize">
                                     {element[getLocalized('description')]}
                                 </p>
                             }
                         </div>
                         <Link
                             href={route('frontend.product.index', {category_id: element.id})}
-                            className="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full"
+                            className="mt-6 flex-shrink-0 flex bg-white bg-opacity-0 py-3 px-4 border border-white border-opacity-25 rounded-md items-center justify-center text-base font-medium text-white hover:bg-opacity-10 sm:mt-0 sm:ml-8 lg:ml-0 lg:w-full capitalize"
                         >
                             {trans('view')}
                         </Link>
