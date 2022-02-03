cf0831aa92107bfbabd407b1a51dcfac14d350b7
uusa35 <uusa35@gmail.com>
2022-02-03 09:15:49 +0300

    updates

--- resources/js/Pages/Backend/BackendHomePage.js
+++ resources/js/Pages/Backend/BackendHomePage.js
@@ -1,10 +1,10 @@
 import {useContext} from 'react'
-import { Link} from "@inertiajs/inertia-react";
-import { AppContext } from './../context/AppContext';
+import {Link} from "@inertiajs/inertia-react";
+import {AppContext} from './../context/AppContext';
 import BackendContainer from "./components/containers/BackendContainer";
 import pluralize from 'pluralize';
 import GlobalContext from "../context/GlobalContext";
-import {isEmpty, map , filter } from "lodash";
+import {isEmpty, map, filter} from "lodash";
 import route from 'ziggy-js';
 import {useSelector} from "react-redux";
 import {
@@ -19,7 +19,7 @@ import {
     Tooltip,
     Legend,
 } from 'chart.js';
-import { Line, Bar , Pie} from 'react-chartjs-2';
+import {Line, Bar, Pie} from 'react-chartjs-2';
 
 ChartJS.register(
     CategoryScale,
@@ -33,13 +33,13 @@ ChartJS.register(
     Legend
 );
 
-export default function BackendHomePage({ previousYearChart, currentYearChart  }) {
-    const {trans , getLocalized, getThumb, classNames, arFont , enFont  } = useContext(AppContext);
-    const { auth, settings  } = useContext(GlobalContext);
-    const { modules} = useSelector(state => state);
+export default function BackendHomePage({previousYearChart, currentYearChart}) {
+    const {trans, getLocalized, getThumb, classNames, arFont, enFont} = useContext(AppContext);
+    const {auth, settings} = useContext(GlobalContext);
+    const {modules} = useSelector(state => state);
 
     const data = {
-        labels : previousYearChart.data.labels,
+        labels: previousYearChart.data.labels,
         datasets: [
             // {
             //     label: 'Dataset 1',
@@ -48,7 +48,7 @@ export default function BackendHomePage({ previousYearChart, currentYearChart  }
             // },
             {
                 label: 'Dataset 2',
-                data: [10,20,30,40,50, 60],
+                data: [10, 20, 30, 40, 50, 60],
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
             },
         ],
@@ -73,10 +73,12 @@ export default function BackendHomePage({ previousYearChart, currentYearChart  }
                                         <div className="sm:flex sm:items-center sm:justify-between">
                                             <div className="sm:flex sm:space-x-5">
                                                 <div className="flex-shrink-0">
-                                                    <img className="mx-10 h-20 w-20 rounded-full shadow-md" src={getThumb(auth.image)}
+                                                    <img className="mx-10 h-20 w-20 rounded-full shadow-md"
+                                                         src={getThumb(auth.image)}
                                                          alt=""/>
                                                 </div>
-                                                <div className="mt-4 text-center sm:mt-0 sm:pt-1 rtl:text-right ltr:text-left">
+                                                <div
+                                                    className="mt-4 text-center sm:mt-0 sm:pt-1 rtl:text-right ltr:text-left">
                                                     <p className="text-sm font-medium text-gray-600">{trans('welcome')}</p>
                                                     <p className="text-xl font-bold text-gray-900 sm:text-2xl">{auth[getLocalized()]}</p>
                                                     <p className="text-sm font-medium text-gray-600">{auth.role[getLocalized()]}</p>
@@ -99,23 +101,24 @@ export default function BackendHomePage({ previousYearChart, currentYearChart  }
                     </div>
 
                     {
-                        settings.enable_products && <div className={`bg-white shadow-md rounded-md p-8 py-12 my-6`}>
-                            <h2 className={`text-lg font-medium text-gray-900`}>{trans('sales')}</h2>
-                            <div className=" hidden lg:flex flex-row justify-evenly items-center h-auto">
-                                {/*<div className="flex-1 w-1/3 rounded-md shadow-md p-3 ml-5 ">*/}
-                                {/*    <Line options={previousYearChart.options} data={previousYearChart} className={`${arFont} text-sm font-bold `}/>*/}
-                                {/*</div>*/}
-                                <div className="w-1/3 rounded-md shadow-md p-3 ">
-                                    <Line options={previousYearChart.options} data={previousYearChart.data} />
+                        settings.enable_products && !isEmpty(currentYearChart) && !isEmpty(previousYearChart) &&
+                            <div className={`bg-white shadow-md rounded-md p-8 py-12 my-6`}>
+                                <h2 className={`text-lg font-medium text-gray-900`}>{trans('sales')}</h2>
+                                <div className=" hidden lg:flex flex-row justify-evenly items-center h-auto">
+                                    {/*<div className="flex-1 w-1/3 rounded-md shadow-md p-3 ml-5 ">*/}
+                                    {/*    <Line options={previousYearChart.options} data={previousYearChart} className={`${arFont} text-sm font-bold `}/>*/}
+                                    {/*</div>*/}
+                                    <div className="w-1/3 rounded-md shadow-md p-3 ">
+                                        <Line options={previousYearChart.options} data={previousYearChart.data}/>
+                                    </div>
+                                    <div className="w-1/3 rounded-md shadow-md p-3 ">
+                                        <Line options={currentYearChart.options} data={currentYearChart.data}/>
+                                    </div>
+                                    {/*<div className="w-1/3 rounded-md shadow-md p-3 ">*/}
+                                    {/*    <Pie options={previousYearChart.options} data={previousYearChart.data} />*/}
+                                    {/*</div>*/}
                                 </div>
-                                <div className="w-1/3 rounded-md shadow-md p-3 ">
-                                    <Line options={currentYearChart.options} data={currentYearChart.data} />
-                                </div>
-                                {/*<div className="w-1/3 rounded-md shadow-md p-3 ">*/}
-                                {/*    <Pie options={previousYearChart.options} data={previousYearChart.data} />*/}
-                                {/*</div>*/}
                             </div>
-                        </div>
                     }
 
                     <div className={`bg-white shadow-md rounded-md p-4 my-4`}>
@@ -124,20 +127,24 @@ export default function BackendHomePage({ previousYearChart, currentYearChart  }
                             {trans('all_modules_message')}
                         </p>
                         <ul role="list" className="py-6 grid grid-cols-1 gap-6 sm:grid-cols-3 md:grid-cols-4 ">
-                            {!isEmpty(modules) && map(filter(modules,  m =>m.index && m.on_top && !m.hide_module && route().has(`backend.${m.name}.index`)),(m) => (
+                            {!isEmpty(modules) && map(filter(modules, m => m.index && m.on_top && !m.hide_module && route().has(`backend.${m.name}.index`)), (m) => (
                                 <li key={m.name} className={`flow-root rounded-md px-2  bg-white`}>
-                                    <div className={`relative -m-2 p-2 flex items-center space-x-4 rounded-xl hover:bg-gray-100 focus-within:ring-2 focus-within:ring-gray-500`}>
+                                    <div
+                                        className={`relative -m-2 p-2 flex items-center space-x-4 rounded-xl hover:bg-gray-100 focus-within:ring-2 focus-within:ring-gray-500`}>
                                         <div
                                             className={classNames(
                                                 'flex-shrink-0 flex items-center justify-center h-16 w-16 rounded-lg'
                                             )}
                                         >
-                                            <img className={`rtl:ml-3 ltr:mr-3 rounded-md w-14 h-auto`} src={getThumb(m.image)} alt={m.name}/>
+                                            <img className={`rtl:ml-3 ltr:mr-3 rounded-md w-14 h-auto`}
+                                                 src={getThumb(m.image)} alt={m.name}/>
                                         </div>
                                         <div>
                                             <h3 className={`text-sm font-medium text-gray-900`}>
-                                                <Link href={m.main_menu ? `/backend/${m.name}/search` : `/backend/${m.name}`} className="focus:outline-none">
-                                                    <span className="absolute inset-0" aria-hidden="true" />
+                                                <Link
+                                                    href={m.main_menu ? `/backend/${m.name}/search` : `/backend/${m.name}`}
+                                                    className="focus:outline-none">
+                                                    <span className="absolute inset-0" aria-hidden="true"/>
                                                     {trans(pluralize(m.name))}
                                                 </Link>
                                             </h3>
