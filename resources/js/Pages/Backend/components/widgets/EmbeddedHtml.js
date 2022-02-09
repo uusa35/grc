e9968619255274b1e18f0d52a14d2969634a78c1
uusa35 <uusa35@gmail.com>
2022-02-09 11:16:59 +0300

    updates

--- resources/js/Pages/Backend/components/widgets/EmbeddedHtml.js
+++ resources/js/Pages/Backend/components/widgets/EmbeddedHtml.js
@@ -1,7 +1,11 @@
 import PropTypes from 'prop-types';
+import {AppContext} from "../../../context/AppContext";
+import {useContext} from "react";
 
-export default function EmbeddedHtml({ html }) {
-    return <div  className="overflow-hidden w-full h-auto" dangerouslySetInnerHTML={{__html: html}}></div>;
+export default function EmbeddedHtml({html}) {
+    const {mainColor} = useContext(AppContext)
+    return <div className={`overflow-hidden w-full h-auto text-${mainColor}-900 dark:text-${mainColor}-50`}
+                dangerouslySetInnerHTML={{__html: html}}></div>;
 }
 
 EmbeddedHtml.propTypes = {
