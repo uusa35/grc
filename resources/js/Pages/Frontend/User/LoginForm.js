5761d83a7dbcfb2fbf382fbe4429addc9774fc2d
uusa35 <uusa35@gmail.com>
2022-02-10 13:41:53 +0300

    updates

--- resources/js/Pages/Frontend/User/LoginForm.js
+++ resources/js/Pages/Frontend/User/LoginForm.js
@@ -46,7 +46,7 @@ export default function() {
 
     return (
         <FrontendContainer>
-            <FrontendContentContainer parentModuleName={'logging'}>
+            <FrontendContentContainer parentModuleName={'login'}>
                 <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
                     <div className="sm:mx-auto sm:w-full sm:max-w-md">
                         <img
