c475277db1f0129f131892847dcf13c244bf2bc8
uusa35 <uusa35@gmail.com>
2022-02-18 23:08:50 +0300

    updates

--- resources/js/Pages/Frontend/Product/FrontendProductShow.js
+++ resources/js/Pages/Frontend/Product/FrontendProductShow.js
@@ -293,7 +293,6 @@ export default function({element, relatedElements, auth, settings}) {
                                                                 value={attribute.color_id}
                                                                 className={({active, checked}) =>
                                                                     classNames(
-                                                                        attribute.color,
                                                                         active && checked ? `ring-2 ring-offset-1 ring-${mainColor}-100 dark:ring-${mainColor}-400` : `ring-2 ring-offset-1 ring-${mainColor}-100 dark:ring-${mainColor}-400`,
                                                                         !active && checked ? `ring-2 ring-${mainColor}-100 dark:ring-${mainColor}-800` : `ring-2 ring-${mainColor}-200 dark:ring-${mainColor}-600`,
                                                                         `-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none hover:bg-${mainBgColor}-100 dark:hover:bg-${mainBgColor}-800`
@@ -335,8 +334,8 @@ export default function({element, relatedElements, auth, settings}) {
                                                                         attribute.size ? 'cursor-pointer focus:outline-none' : 'opacity-25 cursor-not-allowed',
                                                                         active ? `ring-2 ring-offset-2 ring-${mainBgColor}-200 dark:ring-${mainBgColor}-800` : '',
                                                                         checked
-                                                                            ? `bg-${mainBgColor}-800 dark:bg-${mainBgColor}-600 border-transparent text-${mainColor}-800 dark:text-${mainColor}-50 hover:bg-${mainBgColor}-300 dark:hover:bg-${mainBgColor}-400`
-                                                                            : `bg-${mainBgColor}-600 dark:bg-${mainBgColor}-600 border-gray-200 text-${mainColor}-800 dark:text-${mainColor}-200 hover:bg-${mainBgColor}-600 dark:hover:bg-${mainBgColor}-800`,
+                                                                            ? `bg-${mainBgColor}-400 dark:bg-${mainBgColor}-400 border-transparent text-${mainColor}-800 dark:text-${mainColor}-50 hover:bg-${mainBgColor}-400 dark:hover:bg-${mainBgColor}-400`
+                                                                            : `bg-${mainBgColor}-400 dark:bg-${mainBgColor}-400 border-gray-200 text-${mainColor}-800 dark:text-${mainColor}-200 hover:bg-${mainBgColor}-400 dark:hover:bg-${mainBgColor}-400`,
                                                                         'border rounded-md py-3 px-3 flex items-center justify-center text-xs font-medium uppercase sm:flex-1 truncate'
                                                                     )
                                                                 }
