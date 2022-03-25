c7d2e39fba9cef0114001aee6ff49a10fd255663
uusa35 <uusa35@gmail.com>
2022-03-25 16:59:22 +0300

    updates

--- resources/js/Pages/Backend/Category/CategoryIndex.js
+++ resources/js/Pages/Backend/Category/CategoryIndex.js
@@ -62,17 +62,17 @@ export default function({elements}) {
                                             </div>
                                             {isAdminOrAbove ?
                                                 <div className="flex items-center justify-center">
-                                                    {/*<Link*/}
-                                                    {/*    className={`pl-3 has-tooltip`}*/}
-                                                    {/*    href={route('backend.category.create', {user_id: elements.data.length > 0 ? elements.data[0]?.user_id : params.user_id})}>*/}
-                                                    {/*    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"*/}
-                                                    {/*         fill="none" viewBox="0 0 24 24" stroke="currentColor">*/}
-                                                    {/*        <path strokeLinecap="round" strokeLinejoin="round"*/}
-                                                    {/*              strokeWidth="2"*/}
-                                                    {/*              d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>*/}
-                                                    {/*    </svg>*/}
-                                                    {/*    <ToolTipWidget message={trans('create')}/>*/}
-                                                    {/*</Link>*/}
+                                                    <Link
+                                                        className={`pl-3 has-tooltip`}
+                                                        href={route('backend.category.create', {user_id: elements.data.length > 0 ? elements.data[0]?.user_id : params.user_id})}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6"
+                                                             fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                                  strokeWidth="2"
+                                                                  d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+                                                        </svg>
+                                                        <ToolTipWidget message={trans('create')}/>
+                                                    </Link>
                                                     <a
                                                         className={`pl-3 has-tooltip`}
                                                         title={trans('pdf')}
