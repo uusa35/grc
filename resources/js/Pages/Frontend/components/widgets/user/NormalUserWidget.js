e5deab5c62d5cfaf773eec1448018ba2f11f19d0
uusa35 <uusa35@gmail.com>
2022-03-31 14:00:30 +0300

    updates

--- resources/js/Pages/Frontend/components/widgets/user/NormalUserWidget.js
+++ resources/js/Pages/Frontend/components/widgets/user/NormalUserWidget.js
@@ -14,17 +14,17 @@ const NormalUserWidget =  ({ element }) => {
         >
         <Link href={route('frontend.user.show', element.id) + `?slug=${element[getLocalized()].replace(/ /g, '-')}`}
               className="block relative rounded overflow-hidden z-0">
-            <div className="w-auto h-auto flex items-center justify-center aspect-w-1 aspect-h-1 ">
+            <div className="w-auto h-auto flex items-center justify-center aspect-w-1 aspect-h-1 mb-4">
                 <img
                     src={getThumb(element.image)}
                     alt={element[getLocalized()]}
-                    className="z-0 w-4/4 h-auto rounded-full shadow-md object-cover group-hover:opacity-75"
+                    className="z-0 w-4/4 h-auto rounded-full shadow-md object-center object-cover group-hover:opacity-75"
                     width={360}
                     height={480}
                     loading='lazy'
                 />
             </div>
-            <h3 className={`mt-4 text-center text-${mainColor}-800 dark:text-${mainColor}-100 truncate text-sm`}>
+            <h3 className={`text-center text-${mainColor}-800 dark:text-${mainColor}-100 truncate text-sm`}>
                 {truncate(element[getLocalized()], { length : 25 })}
             </h3>
         </Link>
