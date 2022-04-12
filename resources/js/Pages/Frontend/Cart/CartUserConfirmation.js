bb36a8639fe841e72036ed3084d1932d554c9021
uusa35 <uusa35@gmail.com>
2022-04-12 11:00:21 +0300

    updates

--- resources/js/Pages/Frontend/Cart/CartUserConfirmation.js
+++ resources/js/Pages/Frontend/Cart/CartUserConfirmation.js
@@ -51,6 +51,9 @@ export default function({countries, auth}) {
         const selectedCountry = data.country_id ? first(filter(countries, c => c.id == data.country_id)) : first(filter(countries, c => c.id == auth.country_id));
         setGovernates(selectedCountry.governates);
         const governate = first(filter(selectedCountry.governates, g => g.id == auth.governate_id));
+        console.log('the governate', governate);
+        console.log('the governate areas', governate.areas);
+        debugger;
         setAreas(governate.areas);
     }, [])
 
