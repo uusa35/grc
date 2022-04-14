0bf6651db700287561bd513206f010661c437e67
uusa35 <uusa35@gmail.com>
2022-04-14 17:07:56 +0300

    updates

--- resources/js/Pages/Frontend/Course/FrontendCourseShow.js
+++ resources/js/Pages/Frontend/Course/FrontendCourseShow.js
@@ -17,7 +17,7 @@ import ElementRating from "../components/widgets/ElementRating";
 import ElementFavoriteBtn from "../components/widgets/ElementFavoriteBtn";
 import {isMobile} from "react-device-detect";
 import {useForm} from "@inertiajs/inertia-react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {checkCartBeforeAdd} from "../../redux/actions";
 import AlertMessage from "../partials/AlertMessage";
 import EmbeddedHtml from "../../Backend/components/widgets/EmbeddedHtml";
@@ -33,6 +33,7 @@ export default function({element, relatedElements, auth}) {
     const { settings } = useContext(GlobalContext);
     const [selectedTiming, setSelectedTiming] = useState();
     const [currentImages, setCurrentImages] = useState([]);
+    const { locale } = useSelector(state => state);
     const dispatch = useDispatch();
     const {data, setData, post, progress} = useForm({
         'type': 'course',
@@ -112,7 +113,63 @@ export default function({element, relatedElements, auth}) {
                             />
                             <ImageGallery
                                 showBullets={true}
-                                showNav={false}
+                                showNav={true}
+                                renderLeftNav={(onClick, disabled) => <button
+                                    type="button"
+                                    className="image-gallery-icon image-gallery-left-nav"
+                                    disabled={disabled}
+                                    onClick={onClick}
+                                    aria-label="Previous Slide"
+                                >
+                                    {locale.isRTL ?
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"/>
+                                        </svg>
+
+                                        :
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7"/>
+                                        </svg>
+                                    }
+
+                                </button>}
+                                renderRightNav={(onClick, disabled) => <button
+                                    type="button"
+                                    className="image-gallery-icon image-gallery-right-nav"
+                                    disabled={disabled}
+                                    onClick={onClick}
+                                    aria-label="Previous Slide"
+                                >
+                                    {locale.isRTL ?
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"/>
+                                        </svg>
+
+                                        :
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                             viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"/>
+                                        </svg>
+                                    }
+
+                                </button>}
+                                renderFullscreenButton={(onClick, disabled) => <button
+                                    type="button"
+                                    className="image-gallery-icon image-gallery-fullscreen-button"
+                                    disabled={disabled}
+                                    onClick={onClick}
+                                    aria-label="Previous Slide"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7"/>
+                                    </svg>
+                                </button>
+                                }
                                 originalAlt={element[getLocalized()]}
                                 originalTitle={element[getLocalized()]}
                                 thumbnailLabel={element[getLocalized()]}
